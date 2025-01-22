9c84e8ec43454b7852bc8b110efe168a08628304
gn-t-k <gntk1995422@gmail.com>
2025-01-22 15:59:33 +0900

    wip

--- apps/web2/vite.config.ts
+++ apps/web2/vite.config.ts
@@ -1,5 +1,5 @@
-import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -14,7 +14,7 @@ export default defineConfig(({ isSsrBuild }) => ({
 	},
 	ssr: {
 		target: "webworker",
-		noExternal: true,
+		// noExternal: true,
 		external: ["node:async_hooks"],
 		resolve: {
 			conditions: ["workerd", "browser"],
@@ -31,14 +31,21 @@ export default defineConfig(({ isSsrBuild }) => ({
 		},
 	},
 	plugins: [
-		vitePluginViteNodeMiniflare({
-			entry: "./workers/app.ts",
-			miniflareOptions: (options) => {
-				options.compatibilityDate = "2024-11-18";
-				options.compatibilityFlags = ["nodejs_compat"];
-				options.d1Databases = { db: "58e2d047-3151-4e2f-9b25-a035c00c6296" };
-				// match where wrangler applies migrations to
-				options.d1Persist = "../../packages/database/.wrangler/state/v3/d1";
+		// vitePluginViteNodeMiniflare({
+		// 	entry: "./workers/app.ts",
+		// 	miniflareOptions: (options) => {
+		// 		options.compatibilityDate = "2024-11-18";
+		// 		options.compatibilityFlags = ["nodejs_compat"];
+		// 		options.d1Databases = { db: "58e2d047-3151-4e2f-9b25-a035c00c6296" };
+		// 		// match where wrangler applies migrations to
+		// 		options.d1Persist = "../../packages/database/.wrangler/state/v3/d1";
+		// 	},
+		// }),
+		cloudflareDevProxy({
+			getLoadContext: ({ context }) => {
+				return {
+					cloudflare: context.cloudflare,
+				};
 			},
 		}),
 		reactRouter(),
