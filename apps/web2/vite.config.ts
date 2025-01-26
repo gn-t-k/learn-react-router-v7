f01b38691536aa4297c5cbdbb484bb0189682821
gn-t-k <gntk1995422@gmail.com>
2025-01-26 18:02:42 +0900

    wip

--- apps/web2/vite.config.ts
+++ apps/web2/vite.config.ts
@@ -1,44 +1,58 @@
+import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import serverAdapter from "hono-react-router-adapter/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const entry = "./workers/app.ts";
+
 export default defineConfig(({ isSsrBuild }) => ({
 	build: {
-		rollupOptions: isSsrBuild
-			? {
-					input: "./workers/app.ts",
-				}
-			: undefined,
+		rollupOptions: {
+			input: isSsrBuild ? entry : undefined,
+		},
 	},
+	// ssr: {
+	// 	target: "webworker",
+	// 	noExternal: true,
+	// 	external: ["node:async_hooks"],
+	// 	resolve: {
+	// 		conditions: ["workerd", "browser"],
+	// 	},
+	// 	optimizeDeps: {
+	// 		include: [
+	// 			"react",
+	// 			"react/jsx-runtime",
+	// 			"react/jsx-dev-runtime",
+	// 			"react-dom",
+	// 			"react-dom/server",
+	// 			"react-router",
+	// 		],
+	// 	},
+	// },
 	ssr: {
-		target: "webworker",
-		// noExternal: true,
-		external: ["node:async_hooks"],
 		resolve: {
-			conditions: ["workerd", "browser"],
+			conditions: ["workerd", "worker", "browser"],
+			externalConditions: ["workerd", "worker"],
 		},
-		optimizeDeps: {
-			include: [
-				"react",
-				"react/jsx-runtime",
-				"react/jsx-dev-runtime",
-				"react-dom",
-				"react-dom/server",
-				"react-router",
-			],
-		},
+	},
+	resolve: {
+		mainFields: ["browser", "module", "main"],
 	},
 	plugins: [
-		cloudflareDevProxy({
-			getLoadContext: ({ context }) => {
-				return {
-					cloudflare: context.cloudflare,
-				};
-			},
-		}),
 		reactRouter(),
+		// cloudflareDevProxy({
+		// 	getLoadContext: ({ context }) => {
+		// 		return {
+		// 			cloudflare: context.cloudflare,
+		// 		};
+		// 	},
+		// }),
+		serverAdapter({
+			adapter,
+			entry,
+		}),
 		tsconfigPaths(),
 		vanillaExtractPlugin(),
 	],
